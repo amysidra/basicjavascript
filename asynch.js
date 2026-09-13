b0f8799351a0d51f53956ea59b87a5e4c86dab63
amysidra <amysidra@gmail.com>
2026-09-13 11:38:40 +0700

    commit baru

---
+++ asynch.js
@@ -0,0 +1,27 @@
+// variable url
+const products = "https://fakestoreapi.com/products";
+const users = "https://fakestoreapi.com/users";
+
+// function
+async function getData(url) {
+    try {
+        const response = await fetch(url)
+        const dataJson = await response.json()
+        return dataJson
+    } catch (error) {
+        console.log("disini ada error di function pertama")
+    }
+}
+
+// function 
+async function listData(url) {
+    try {
+        const lists = await getData(url)
+        return lists
+    } catch (error) {
+        console.log("disini error function 2")
+    }
+}
+
+const listproduct = await listData(products)
+console.log(listproduct)
